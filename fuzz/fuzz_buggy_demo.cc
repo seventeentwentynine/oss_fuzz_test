96d7e9d483526dd85b9f3d7ed4cc1c8ae7829ada
seventeentwentynine <50687106+seventeentwentynine@users.noreply.github.com>
2025-11-24 11:05:33 -0800

    Starting Fuzz

--- fuzz/fuzz_buggy_demo.cc
+++ fuzz/fuzz_buggy_demo.cc
@@ -1,6 +1,6 @@
 #include <cstddef>
 #include <cstdint>
-#include "buggy_demo.h"
+#include "../src/buggy_demo.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     demo::process_input(data, size);
